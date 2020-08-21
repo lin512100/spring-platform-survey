673cf90e5647311370ef5267a7ab48283e110a79
lin512100 <1290364802@qq.com>
2020-08-21 11:09:58 +0800

    【update】添加日志线程池和日志异步提交

--- spring-platform-cloud-common/src/main/java/com/jtang/common/service/LogService.java
+++ spring-platform-cloud-common/src/main/java/com/jtang/common/service/LogService.java
@@ -1,11 +1,13 @@
 package com.jtang.common.service;
 
 import com.jtang.common.model.common.SysLog;
+import org.springframework.scheduling.annotation.Async;
 
 public interface LogService {
 
     /**
      * 日志新增
      * */
+    @Async("logExecutor")
     void insert(SysLog sysLog);
 }
--- spring-platform-cloud-common/src/main/java/com/jtang/common/service/impl/DefaultLogServciceImpl.java
+++ spring-platform-cloud-common/src/main/java/com/jtang/common/service/impl/DefaultLogServciceImpl.java
@@ -3,12 +3,15 @@ package com.jtang.common.service.impl;
 import com.jtang.common.model.common.SysLog;
 import com.jtang.common.service.LogService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class DefaultLogServciceImpl implements LogService {
 
     @Override
     public void insert(SysLog sysLog) {
-      log.info(sysLog.toString());
+        log.info(sysLog.toString());
     }
 }
