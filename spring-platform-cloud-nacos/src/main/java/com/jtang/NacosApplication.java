656308d89f6070d09bda403721afff275e45e998
huanglian <726411951@qq.com>
2020-08-26 16:02:36 +0800

    [add]springcloud-alibaba nacos-config

---
+++ spring-platform-cloud-nacos/src/main/java/com/jtang/NacosApplication.java
@@ -0,0 +1,13 @@
+package com.jtang;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huanglian
+ * @date 2020/8/26
+ * @description nacos注册中心
+ */
+@SpringBootApplication
+public class NacosApplication {
+
+}
