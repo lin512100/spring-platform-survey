aa8a9e184615af0bf7a478763d3a922b93f4e60f
lin512100 <1290364802@qq.com>
2020-09-28 00:09:35 +0800

    重构进行中

--- spring-platform-cloud-account/src/main/java/com/jtang/AccountApplication.java
+++ spring-platform-cloud-account/src/main/java/com/jtang/AccountApplication.java
@@ -1,4 +1,4 @@
-package com.jtang.account;
+package com.jtang;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
--- spring-platform-cloud-account/src/main/java/com/jtang/config/ResourceServerConfig.java
+++ spring-platform-cloud-account/src/main/java/com/jtang/config/ResourceServerConfig.java
@@ -1,4 +1,4 @@
-package com.jtang.account.config;
+package com.jtang.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
